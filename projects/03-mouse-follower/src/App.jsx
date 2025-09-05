8458b94874efadda6c2ca83417e233ea7525629e
xaxo <jbaigorri@hotmail.com>
2025-09-05 16:39:39 +0200

    Desagrupar elementos

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,10 +1,69 @@
+import  { useEffect, useState } from "react"
 
 
-function App() {
+const FollowMouse = () => {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({x:0,y:0})
+
+  useEffect(()=>{
+    console.log('enabled:', enabled)
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({x: clientX, y: clientY })
+      console.log(`${clientX} : ${clientY}`)
+    }
+
+    if(enabled) {
+      console.log('limpiado')
+      window.addEventListener('pointermove', handleMove) 
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+      console.log('Limpiado')
+    }
+  
+  },[enabled])
+
+  const handClick = () => {
+    setEnabled(!enabled)
+  }
 
   return (
-    <h3>Projecto 03</h3>
+    <>
+        <div 
+          style={
+              {
+                position: "absolute",
+                backgroundColor: 'rgba(0, 0, 0, 0.5)',
+                border: '1px solid #09f',
+                borderRadius: '50%' ,
+                opacity: 0.8,
+                pointerEvents: 'none',
+                left: -20,
+                top: -20,
+                width: 40,
+                height: 40,
+                transform: `translate(${position.x}px, ${position.y}px`
+              }}
+        />
+        <button onClick={handClick}>{enabled ? 'Desactivar' : 'Activar'} Seguir Puntero</button>
+    </>
+  )  
+}
 
+function App() {
+  const [mounted, setMounted] = useState(true)
+
+
+  return (
+    <main>
+      {mounted &&  <FollowMouse />  }
+      <br />
+      <button onClick={() => setMounted(!mounted)}>
+        Touggle mounted, FollowMouse component
+      </button>
+    </main>
   )
 }
 
